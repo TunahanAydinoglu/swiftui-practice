07d732376f65114bd9c7436d083224bc4b6a2775
Tunahan <tunahannaydinoglu@gmail.com>
2023-04-19 23:21:07 +0300

    initial commit

---
+++ SwiftUIPractice/Features/Authentication/Welcome/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  SwiftUIPractice
+//
+//  Created by Tunahan Aydınoglu on 19.04.2023.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Tunahan Aydınoglu on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
